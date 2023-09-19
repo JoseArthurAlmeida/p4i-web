43a92839d1b06fbbbd359be61f9a1357caaf3c7b
José Arthur <almeida.arthur@academico.ifpb.edu.br>
2023-09-19 08:54:59 -0300

    implementação do cadastro de reuniões

--- src/routes/pages/reuniao/Cadastroreuniao.jsx
+++ src/routes/pages/reuniao/Cadastroreuniao.jsx
@@ -1,7 +1,11 @@
-import {Layout} from 'antd';
+import { useState} from "react";
+import { Layout } from 'antd';
 import '../../../css/cadastroreu.css'
 //import { NavLink } from 'react-router-dom';
-const {Content} = Layout;
+import { useNavigate } from "react-router-dom";
+import conexaoApi from "../../../axios/config";
+
+const { Content } = Layout;
 
 /* requisao post
 
@@ -10,36 +14,80 @@ const {Content} = Layout;
     "titulo": "requisicao post",
     "objetivo": "ver funcioinamento",
     "ata": "esta funcionando?",
-     "participantes": "d, js, ja"
+    "participantes": "d, js, ja"
   }
 }
 */
 const Cadastroreuniao = () => {
-    return (
+  const navigate = useNavigate();
+  const [titulo, setTitulo] = useState("");
+  const [data_realizacao, setData] = useState("");
+  const [participantes, setParticipantes] = useState("");
+  const [objetivo, setObjetivo] = useState("");
+  const [ata, setAta] = useState("");
+
+  function mudaEstadoTitulo(event) {
+    const novoTitulo = event.target.value;
+    setTitulo(novoTitulo);
+  }
+
+  function mudaEstadoData(event) {
+    const novoData = event.target.value;
+    setData(novoData);
+  }
+
+  function mudaEstadoParticipantes(event) {
+    const novoParticipantes = event.target.value;
+    setParticipantes(novoParticipantes);
+  }
+
+  function mudaEstadoObjetivo(event) {
+    const novoObjetivo = event.target.value;
+    setObjetivo(novoObjetivo);
+  }
+  function mudaEstadoAta(event) {
+    const novoAta = event.target.value;
+    setAta(novoAta);
+  }
+
+  const cadastrarReuniao = async (event) => {
+    event.preventDefault();
+
+    const reuniao = { titulo, data_realizacao, participantes, objetivo, ata };
+    console.log(reuniao);
+
+    await conexaoApi.post("/reuniaos", { data: reuniao });
+    navigate("/reunioesinseri")
+  }
+
+
+
+  return (
     <Layout>
-        <Content>
-            <form id='form'>
-              <div class='conteudo-form'><h class='titulos'>Título:</h>
-              <input class='inputcadastro' id='titulo' placeholder='Dê um título a sua reunião'/></div>
+      <Content>
+        <form id='form' onSubmit={(e) => cadastrarReuniao(e)}>
+          <div class='conteudo-form'><h class='titulos'>Título:</h>
+            <input class='inputcadastro' value={titulo} onChange={mudaEstadoTitulo} id='titulo' placeholder='Dê um título a sua reunião' /></div>
 
-              <div class='conteudo-form'><h class='titulos'>Data:</h>
-              <input class='inputcadastro' id='data' type='date'/></div>
+          <div class='conteudo-form'><h class='titulos'>Data:</h>
+            <input class='inputcadastro' value={data_realizacao} onChange={mudaEstadoData} id='data' type='date' /></div>
 
-              <div class='conteudo-form'><h  class='titulos'>Participantes:</h>
-              <input class='inputcadastro' id='participantes' placeholder='Informe os participantes'/></div>
+          <div class='conteudo-form'><h class='titulos'>Participantes:</h>
+            <input class='inputcadastro' value={participantes} onChange={mudaEstadoParticipantes} id='participantes' placeholder='Informe os participantes' /></div>
 
-              <div class='conteudo-form'><h class='titulos'>Objetivo:</h>
-              <textarea class='inputcadastro' id="objetivo" name="objetivo" placeholder='Descreva o objetivo da reunião realizada' style={{width: '300px', height: '30px', font: 'Arial'}}></textarea></div>
+          <div class='conteudo-form'><h class='titulos'>Objetivo:</h>
+            <textarea class='inputcadastro' value={objetivo} onChange={mudaEstadoObjetivo} id="objetivo" name="objetivo" placeholder='Descreva o objetivo da reunião realizada' style={{ width: '300px', height: '30px', font: 'Arial' }}></textarea></div>
 
-              <div class='conteudo-form'><h class='titulos'>Ata:</h>
-              <input class='inputcadastro' id='ata'/></div>
-            </form>
+          <div class='conteudo-form'><h class='titulos'>Ata:</h>
+            <input class='inputcadastro' value={ata} onChange={mudaEstadoAta} id='ata' /></div>
+          <div id='button-position'><button class="btn-cadastrar-reu">Cadastrar</button></div>
+        </form>
 
-            <div id='button-position'><button class="btn-cadastrar-reu">Cadastrar</button></div>
+      </Content>
 
-        </Content>
-    </Layout>    
-    );
-  };
-  
-  export default Cadastroreuniao;
+
+    </Layout>
+  );
+};
+
+export default Cadastroreuniao;
