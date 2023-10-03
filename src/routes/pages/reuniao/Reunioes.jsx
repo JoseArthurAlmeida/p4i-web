21006298cd599e776642d9578c0215313086acc2
José Arthur <almeida.arthur@academico.ifpb.edu.br>
2023-10-03 10:54:18 -0300

    adição da funcionalidade de apagar reunião

--- src/routes/pages/reuniao/Reunioes.jsx
+++ src/routes/pages/reuniao/Reunioes.jsx
@@ -4,6 +4,7 @@ import '../../../css/atividades-reu.css';
 import botaomais from '../../../images/botaomais.png';
 import conexaoApi from "../../../axios/config";
 import formataData from "../../../components/funcoes_compartilhadas/formatarData";
+import { BiTrash } from "react-icons/bi";
 
 const Reunioes = () => {
 
@@ -21,42 +22,59 @@ const Reunioes = () => {
     }
   };
 
+  // Função excluir reuniao
+  const apagarReuniao = async (id) => {
+    if (window.confirm('Tem certeza que deseja apagar?')) {
+      console.log(id);
+      await conexaoApi.delete(`/reuniaos/${id}`);
+      getReunioes();
+    }
+  }
+
   useEffect(() => {
     getReunioes();
   }, []);
 
 
   return (
-    
+
     <div className='conteudo'>
       {
         reunioes.map((reuniao) => (
           <div className='nav' key={reuniao.id}>
-            <NavLink to={`/reunioes/${reuniao.id}`}>
+
             <button className='btn-atv-reu'>
+
               <div id='titulo-status'>
                 <h className='text-titulo'>{reuniao.attributes.titulo}</h>
               </div>
+
+
+              <div>
+                  <div >
+                    <NavLink to={`/reunioes/${reuniao.id}`}>
+                      <button>Acessar</button>
+                    </NavLink>
+                    <button onClick={() => apagarReuniao(reuniao.id)}><BiTrash size={30} /></button>
+                  </div>
+                </div>
+
+
+
               <div id='conteudo-btn-reu'>
                 <h className='text-data'>Realizada: {formataData(reuniao.attributes.data_realizacao)}</h>
               </div>
             </button>
-            </NavLink>
+
           </div>
         ))
-        }
+      }
+      <NavLink to='/cadastroreunioes'><button id="botao-mais"><img src={botaomais} alt='botaomais' /></button></NavLink>
+
+    </div>
 
 
 
-
-      
-      <NavLink to='/cadastroreunioes'><button id="botao-mais"><img src={botaomais}  alt='botaomais'/></button></NavLink>
-
-      </div>
-    
-
-    
   );
 };
- //to={`/posts/${post.id}`}
 export default Reunioes;
