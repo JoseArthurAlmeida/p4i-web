4e66c4c3cbed7f1d02de983071d1cc923bb04d6c
Arthur <denis.willian@academico.ifpb.edu.br>
2023-10-24 11:09:34 -0300

    ajustes geral em css

--- src/routes/pages/projeto/Projeto.jsx
+++ src/routes/pages/projeto/Projeto.jsx
@@ -108,16 +108,4 @@ const Inseri = () => {
     </main>
   )
 };
-
-/**
- *<main className='main-transparent'>
-          <div id='conteudo-geral'>
-            <div className="clique">
-              <h id='titulo-inseri'>No momento você não está inserido em um projeto.</h><br></br>
-              <hr id='barra-verde' />
-            </div>
-          </div>
-        </main>
- */
-
 export default Inseri;
