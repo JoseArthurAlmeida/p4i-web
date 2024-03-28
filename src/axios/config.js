dbd8183e12c5db285fd93b2fa6d49e7fb3e48a79
José Arthur <almeida.arthur@academico.ifpb.edu.br>
2024-03-28 18:44:43 -0300

    Atualização do link da API

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const conexaoApi = axios.create({
-  //baseURL: "https://backend-p4i.josarthur2.repl.co/api",
   baseURL: 'https://d337eea1-1800-4db1-8a2f-dc1196ea2199-00-3lfsyinh3e6gn.worf.replit.dev/api',
-  
   headers: {
     "Content-Type": "application/json",
   },
